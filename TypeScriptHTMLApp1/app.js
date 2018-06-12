7d528c66c599510e38f1f7c8f7f56058bb17c2bc
dutchwarrior <salute.federico94@gmail.com>
2018-06-12 21:22:57 +0200

    add type example

--- TypeScriptHTMLApp1/app.js
+++ TypeScriptHTMLApp1/app.js
@@ -15,23 +15,77 @@ var Greeter = /** @class */ (function () {
     };
     return Greeter;
 }());
-var Prova = /** @class */ (function () {
-    function Prova() {
+var Listing1_5;
+(function (Listing1_5) {
+    var name = "steve";
+    var heightInCentimeters = 182.88;
+    var isActive = true;
+    //array
+    var names = ['James', 'Nick', 'Rebecca', 'Lily'];
+    Listing1_5.sayHello = function (name) {
+        return 'Hello ' + name;
+    };
+    Listing1_5.person = {
+        name: 'Federico',
+        heightInCentimeters: 180.78
+    };
+})(Listing1_5 || (Listing1_5 = {}));
+var Listing1_6;
+(function (Listing1_6) {
+    Listing1_6.person = {
+        name: 'Federico',
+        heightInCentimeters: 190
+    };
+})(Listing1_6 || (Listing1_6 = {}));
+var Listing1_7;
+(function (Listing1_7) {
+    var monument = [];
+    monument.push({
+        name: 'Statue of Liberty',
+        heightInMeters: 46
+    });
+    monument.push({
+        name: 'Peter the Great',
+        heightInMeters: 96
+    });
+    monument.push({
+        name: 'Angel of the North',
+        heightInMeters: 20
+    });
+    function compareMonumentHeights(a, b) {
+        if (a.heightInMeters > b.heightInMeters) {
+            return -1;
+        }
+        if (a.heightInMeters < b.heightInMeters) {
+            return 1;
+        }
+        return 0;
     }
-    Prova.prototype.setProva = function (prova1) {
-        this.prova1 = prova1;
-    };
-    Prova.prototype.creaAlert = function () {
-        alert(this.prova1);
-    };
-    return Prova;
-}());
+    var monumentsOrderdByHeight = monument.sort(compareMonumentHeights);
+    Listing1_7.tallestMonument = monumentsOrderdByHeight[0];
+})(Listing1_7 || (Listing1_7 = {}));
+var Listing1_8;
+(function (Listing1_8) {
+    var VehicleType;
+    (function (VehicleType) {
+        VehicleType[VehicleType["PedalCycle"] = 0] = "PedalCycle";
+        VehicleType[VehicleType["Car"] = 1] = "Car";
+        VehicleType[VehicleType["Van"] = 2] = "Van";
+        VehicleType[VehicleType["Bus"] = 3] = "Bus";
+    })(VehicleType || (VehicleType = {}));
+    Listing1_8.type = VehicleType.Car;
+    Listing1_8.typeName = VehicleType[Listing1_8.type];
+})(Listing1_8 || (Listing1_8 = {}));
 window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
-    var prova = new Prova();
-    prova.setProva("sono la prima prova");
-    prova.creaAlert();
+    console.log(Listing1_5.sayHello("pippo"));
+    console.log(Listing1_5.person.name + ", " + Listing1_5.person.heightInCentimeters);
+    console.log("nome: " + Listing1_6.person.name + " altezza: " + Listing1_6.person.heightInCentimeters);
+    console.log("Edificio più alto: " + Listing1_7.tallestMonument.name);
+    console.log("Edificio più alto: " + Listing1_7.tallestMonument.name);
+    console.log("Tipo di veicolo: " + Listing1_8.type);
+    console.log("Tipo di veicolo2: " + Listing1_8.typeName);
 };
 //# sourceMappingURL=app.js.map
--- TypeScriptHTMLApp1/app.ts
+++ TypeScriptHTMLApp1/app.ts
@@ -21,25 +21,110 @@
 
 }
 
-class Prova {
 
-    prova1: String;
+module Listing1_5 {
+    var name: string = "steve";
+    var heightInCentimeters: number = 182.88;
+    var isActive: boolean = true;
 
-    setProva(prova1: String) {
-        this.prova1 = prova1;
+    //array
+    var names: string[] = ['James', 'Nick', 'Rebecca', 'Lily'];
+
+    export var sayHello: (name: string) => string;
+
+    sayHello = function (name: string) {
+        return 'Hello ' + name;
+    };
+
+    export var person: { name: string; heightInCentimeters: number; };
+
+     person = {
+        name: 'Federico',
+        heightInCentimeters: 180.78
+    };
+
+}
+
+module Listing1_6 {
+    interface Person {
+        name: string;
+        heightInCentimeters: number;
     }
 
-    creaAlert() {
-        alert(this.prova1);
+    export var person: Person = {
+        name: 'Federico',
+        heightInCentimeters : 190
     }
+
+}
+
+module Listing1_7 {
+
+    interface Monument {
+        name: string;
+        heightInMeters: number;
+    }
+
+    var monument: Monument[] = [];
+
+    monument.push({
+        name: 'Statue of Liberty',
+        heightInMeters : 46
+    });
+
+    monument.push({
+        name: 'Peter the Great',
+        heightInMeters : 96
+    });
+
+    monument.push({
+        name: 'Angel of the North',
+        heightInMeters : 20
+    });
+
+    function compareMonumentHeights(a: Monument, b: Monument) {
+
+        if (a.heightInMeters > b.heightInMeters) {
+            return -1;
+        }
+
+        if (a.heightInMeters < b.heightInMeters) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    var monumentsOrderdByHeight = monument.sort(compareMonumentHeights);
+    export var tallestMonument = monumentsOrderdByHeight[0];
+
+}
+
+module Listing1_8 {
+
+    enum VehicleType {
+        PedalCycle,
+        Car,
+        Van,
+        Bus
+    }
+
+    export var type: VehicleType = VehicleType.Car;
+    export var typeName = VehicleType[type];
 }
 
 window.onload = () => {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
-    var prova = new Prova();
-    prova.setProva("sono la prima prova");
-    prova.creaAlert();
 
+    console.log(Listing1_5.sayHello("pippo"));
+    console.log(Listing1_5.person.name + ", " + Listing1_5.person.heightInCentimeters);
+    console.log("nome: " + Listing1_6.person.name + " altezza: " + Listing1_6.person.heightInCentimeters);
+    console.log("Edificio più alto: " + Listing1_7.tallestMonument.name);
+    console.log("Edificio più alto: " + Listing1_7.tallestMonument.name);
+    console.log("Tipo di veicolo: " + Listing1_8.type);
+    console.log("Tipo di veicolo2: " + Listing1_8.typeName);
 };
+
+
