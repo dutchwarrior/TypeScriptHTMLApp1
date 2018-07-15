b1f16f62ef8b2ae157a566e5c0a9a7d6ea52b120
dutchwarrior <salute.federico94@gmail.com>
2018-07-15 14:03:15 +0200

    ASXZX

--- TypeScriptHTMLApp1/app.js
+++ TypeScriptHTMLApp1/app.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Greeter = /** @class */ (function () {
     function Greeter(element) {
         this.element = element;
@@ -75,7 +85,209 @@ var Listing1_8;
     })(VehicleType || (VehicleType = {}));
     Listing1_8.type = VehicleType.Car;
     Listing1_8.typeName = VehicleType[Listing1_8.type];
+    var BoxSize;
+    (function (BoxSize) {
+        BoxSize[BoxSize["Small"] = 0] = "Small";
+        BoxSize[BoxSize["Medium"] = 1] = "Medium";
+    })(BoxSize = Listing1_8.BoxSize || (Listing1_8.BoxSize = {}));
+    (function (BoxSize) {
+        BoxSize[BoxSize["Large"] = 2] = "Large";
+        BoxSize[BoxSize["ExtraLarge"] = 3] = "ExtraLarge";
+    })(BoxSize = Listing1_8.BoxSize || (Listing1_8.BoxSize = {}));
 })(Listing1_8 || (Listing1_8 = {}));
+var Listing1_10;
+(function (Listing1_10) {
+    var DiscFlags;
+    (function (DiscFlags) {
+        DiscFlags[DiscFlags["None"] = 0] = "None";
+        DiscFlags[DiscFlags["Drive"] = 1] = "Drive";
+        DiscFlags[DiscFlags["Influence"] = 2] = "Influence";
+        DiscFlags[DiscFlags["Steadiness"] = 4] = "Steadiness";
+        DiscFlags[DiscFlags["Conscientiousness"] = 8] = "Conscientiousness";
+    })(DiscFlags || (DiscFlags = {}));
+    // Using flags
+    var personality = DiscFlags.Drive | DiscFlags.Conscientiousness;
+    // Testing flags
+    // true
+    Listing1_10.hasD = (personality & DiscFlags.Drive) == DiscFlags.Drive;
+    // false
+    Listing1_10.hasI = (personality & DiscFlags.Influence) == DiscFlags.Influence;
+    // false
+    Listing1_10.hasS = (personality & DiscFlags.Steadiness) == DiscFlags.Steadiness;
+    // true
+    Listing1_10.hasC = (personality & DiscFlags.Conscientiousness) == DiscFlags.Conscientiousness;
+})(Listing1_10 || (Listing1_10 = {}));
+var Listing1_11;
+(function (Listing1_11) {
+    var avenueRoad = {
+        bedrooms: 11,
+        bathrooms: 10
+    };
+    Listing1_11.mansion = avenueRoad;
+})(Listing1_11 || (Listing1_11 = {}));
+var Listing1_17;
+(function (Listing1_17) {
+    Listing1_17.truthyString = "Truthy string";
+    Listing1_17.invertedTest = !Listing1_17.truthyString;
+    Listing1_17.truthyTest = !!Listing1_17.truthyString;
+    Listing1_17.falseyTest = !!Listing1_17.falseyString;
+    Listing1_17.prova = function () {
+        if (Listing1_17.truthyString) {
+            console.log("truthyString is not undefinited");
+        }
+        if (!Listing1_17.falseyString) {
+            console.log("falseyString is undefinited");
+        }
+    };
+})(Listing1_17 || (Listing1_17 = {}));
+var Listing1_24;
+(function (Listing1_24) {
+    function getAverage(a, b, c) {
+        var total = a + b + c;
+        var avarage = total / 3;
+        return "This is the average: " + avarage;
+    }
+    var result = getAverage(4, 3, 8);
+})(Listing1_24 || (Listing1_24 = {}));
+var Listing1_25;
+(function (Listing1_25) {
+    function getAverage(a, b, c) {
+        var total = a;
+        var count = 1;
+        total += b;
+        count++;
+        if (c) {
+            total += c;
+            count++;
+        }
+        var avarage = total / count;
+        return "the average is " + avarage + " parametri passati in input: " + count;
+    }
+    Listing1_25.result = getAverage(10, 6);
+    Listing1_25.result2 = getAverage(10, 6, 10);
+})(Listing1_25 || (Listing1_25 = {}));
+var Listing1_26;
+(function (Listing1_26) {
+    function concatene(items, separator, beginAt, endAt) {
+        if (separator === void 0) { separator = ","; }
+        if (beginAt === void 0) { beginAt = 0; }
+        if (endAt === void 0) { endAt = items.length; }
+        var result = "";
+        for (var i = beginAt; i < endAt; i++) {
+            result += items[i];
+            if (i < (endAt - 1)) {
+                result += separator;
+            }
+        }
+        return result;
+    }
+    var items = ["A", "B", "C"];
+    var result = concatene(items);
+    var partialResult = concatene(items, "-", 1);
+})(Listing1_26 || (Listing1_26 = {}));
+var Listing1_27;
+(function (Listing1_27) {
+    function getAverage() {
+        var a = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            a[_i] = arguments[_i];
+        }
+        var total = 0;
+        var count = 0;
+        for (var i = 0; i < a.length; i++) {
+            total += a[i];
+            count++;
+        }
+        var average = total / count;
+        return "this is the average: " + average;
+    }
+    Listing1_27.result = getAverage(2, 4, 6, 8, 10);
+})(Listing1_27 || (Listing1_27 = {}));
+var Listing1_28;
+(function (Listing1_28) {
+    function getAverage(a, b, c) {
+        var total = parseInt(a, 10) + parseInt(b, 10) + parseInt(c, 10);
+        var average = total / 3;
+        return 'The average is ' + average;
+    }
+    var result = getAverage(4, 3, 8); // 5
+})(Listing1_28 || (Listing1_28 = {}));
+var Listing1_29;
+(function (Listing1_29) {
+    var HandlerFactory = /** @class */ (function () {
+        function HandlerFactory() {
+        }
+        HandlerFactory.prototype.getHandler = function (type) {
+            if (type === "Ramdom") {
+                return new RamdomHandler();
+            }
+            else if (type === "Reversed") {
+                return new ReversedHandler();
+            }
+            else {
+                return new Handler();
+            }
+        };
+        return HandlerFactory;
+    }());
+    Listing1_29.HandlerFactory = HandlerFactory;
+    var Handler = /** @class */ (function () {
+        function Handler() {
+        }
+        Handler.prototype.handle = function () {
+            console.log("Handled!");
+        };
+        return Handler;
+    }());
+    Listing1_29.Handler = Handler;
+    var ReversedHandler = /** @class */ (function (_super) {
+        __extends(ReversedHandler, _super);
+        function ReversedHandler() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        ReversedHandler.prototype.handle = function () {
+            console.log("!deldnaH");
+        };
+        return ReversedHandler;
+    }(Handler));
+    Listing1_29.ReversedHandler = ReversedHandler;
+    var RamdomHandler = /** @class */ (function (_super) {
+        __extends(RamdomHandler, _super);
+        function RamdomHandler() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        RamdomHandler.prototype.handle = function () {
+            console.log("daH!denl");
+        };
+        return RamdomHandler;
+    }(Handler));
+    Listing1_29.RamdomHandler = RamdomHandler;
+    var RamdomHandler2 = /** @class */ (function (_super) {
+        __extends(RamdomHandler2, _super);
+        function RamdomHandler2() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        RamdomHandler2.prototype.handle = function () {
+            console.log("daH!denl2");
+        };
+        return RamdomHandler2;
+    }(Handler));
+    Listing1_29.RamdomHandler2 = RamdomHandler2;
+})(Listing1_29 || (Listing1_29 = {}));
+var Listing1_31;
+(function (Listing1_31) {
+    var addNumber = function (a, b) {
+        return a + b;
+    };
+    var addNumber = function (a, b) {
+        return a + b;
+    };
+    var addNumber = function (a, b) { return a + b; };
+})(Listing1_31 || (Listing1_31 = {}));
+var Listing1_32;
+(function (Listing1_32) {
+    Listing1_32.makeNum = function (f, l) { return ({ name: f, surname: l }); };
+})(Listing1_32 || (Listing1_32 = {}));
 window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
@@ -85,7 +297,25 @@ window.onload = function () {
     console.log("nome: " + Listing1_6.person.name + " altezza: " + Listing1_6.person.heightInCentimeters);
     console.log("Edificio più alto: " + Listing1_7.tallestMonument.name);
     console.log("Edificio più alto: " + Listing1_7.tallestMonument.name);
-    console.log("Tipo di veicolo: " + Listing1_8.type);
-    console.log("Tipo di veicolo2: " + Listing1_8.typeName);
+    console.log("Index veicolo: " + Listing1_8.type);
+    console.log("Descrizione veicolo: " + Listing1_8.typeName);
+    console.log("Descrizione Box: " + Listing1_8.BoxSize[Listing1_8.BoxSize.ExtraLarge]);
+    console.log("flal1 :" + Listing1_10.hasD);
+    console.log("flal2 :" + Listing1_10.hasI);
+    console.log("flal3 :" + Listing1_10.hasS);
+    console.log("flal4 :" + Listing1_10.hasC);
+    console.log("mansion : " + Listing1_11.mansion);
+    console.log("invertedTest:" + Listing1_17.invertedTest);
+    console.log("truthyTest:" + Listing1_17.truthyTest);
+    console.log("falseyTest:" + Listing1_17.falseyTest);
+    Listing1_17.prova();
+    console.log(Listing1_25.result);
+    console.log(Listing1_25.result2);
+    console.log(Listing1_27.result);
+    var factory = new Listing1_29.HandlerFactory();
+    console.log(factory.getHandler("Ramdom").handle);
+    console.log(factory.getHandler("Ramdom2").handle);
+    var person = Listing1_32.makeNum("Federico", "Salute");
+    console.log("Nome:" + person.name + ", cognome " + person.surname);
 };
 //# sourceMappingURL=app.js.map
--- TypeScriptHTMLApp1/app.ts
+++ TypeScriptHTMLApp1/app.ts
@@ -111,6 +111,230 @@ module Listing1_8 {
 
     export var type: VehicleType = VehicleType.Car;
     export var typeName = VehicleType[type];
+
+    export enum BoxSize {
+        Small,
+        Medium
+    }
+
+    export enum BoxSize {
+        Large = 2,
+        ExtraLarge
+    }
+}
+
+module Listing1_10 {
+
+    enum DiscFlags {
+        None = 0,
+        Drive = 1,
+        Influence = 2,
+        Steadiness = 4,
+        Conscientiousness = 8
+    }
+
+    // Using flags
+    var personality = DiscFlags.Drive | DiscFlags.Conscientiousness;
+
+    // Testing flags
+
+    // true
+    export var hasD = (personality & DiscFlags.Drive) == DiscFlags.Drive;
+
+    // false
+    export var hasI = (personality & DiscFlags.Influence) == DiscFlags.Influence;
+
+    // false
+    export var hasS = (personality & DiscFlags.Steadiness) == DiscFlags.Steadiness;
+
+    // true
+    export var hasC = (personality & DiscFlags.Conscientiousness) == DiscFlags.Conscientiousness;
+
+}
+
+module Listing1_11 {
+
+    interface House {
+        bedrooms: number;
+        bathrooms: number;
+    }
+
+    interface Mansion {
+        bedrooms: number;
+        bathrooms: number;
+        butlers: number;
+    }
+
+    var avenueRoad: House = {
+        bedrooms: 11,
+        bathrooms: 10
+    }
+
+    export var mansion: Mansion = <Mansion>avenueRoad;
+
+}
+
+module Listing1_17 {
+    export var truthyString = "Truthy string";
+    export var falseyString: string;
+
+    export var invertedTest = !truthyString;
+    export var truthyTest = !!truthyString;
+    export var falseyTest = !!falseyString;
+
+    export var prova = () => {
+
+        if (truthyString) {
+            console.log("truthyString is not undefinited");
+        }
+
+        if (!falseyString) {
+            console.log("falseyString is undefinited");
+        }
+
+    }
+}
+
+module Listing1_24 {
+
+    function getAverage(a: number, b: number, c: number): string {
+        var total = a + b + c;
+        var avarage: number = total / 3;
+        return "This is the average: " + avarage;
+    }
+
+    var result: string = getAverage(4, 3, 8);
+}
+
+
+module Listing1_25 {
+
+    function getAverage(a: number, b: number, c?: number) :string {
+        var total = a;
+        var count = 1;
+        total += b;
+        count++;
+
+        if (c) {
+            total += c;
+            count++;
+        }
+
+        var avarage: number = total / count;
+        return "the average is " + avarage + " parametri passati in input: "+ count;
+    }
+
+    export var result: string = getAverage(10, 6);
+    export var result2: string = getAverage(10, 6, 10);
+}
+
+module Listing1_26 {
+    function concatene(items: String[], separator = ",", beginAt = 0, endAt = items.length): string {
+        var result: string = "";
+
+        for (var i = beginAt; i < endAt; i++) {
+            result += items[i];
+            if (i < (endAt - 1)) {
+                result += separator;
+            }
+        }
+
+        return result;
+    }
+
+    var items : string[]= ["A", "B", "C"];
+    var result: string = concatene(items);
+    var partialResult = concatene(items, "-", 1 );
+}
+
+module Listing1_27 {
+
+    function getAverage(...a: number[]) :string {
+        var total: number = 0;
+        var count: number = 0;
+
+        for (var i = 0; i <a.length; i++) {
+            total += a[i];
+            count++;
+        }
+
+        var average = total / count;
+        return "this is the average: " + average;
+    }
+
+    export var result = getAverage(2, 4, 6, 8, 10);
+}
+
+module Listing1_28 {
+
+    function getAverage(a: string, b: string, c: string): string;
+    function getAverage(a: number, b: number, c: number): string;
+
+    function getAverage(a: any, b: any, c: any): string {
+        var total = parseInt(a, 10) + parseInt(b, 10) + parseInt(c, 10);
+        var average = total / 3;
+        return 'The average is ' + average;
+    }
+
+    var result = getAverage(4, 3, 8); // 5
+}
+
+module Listing1_29 {
+    export class HandlerFactory {
+        getHandler(type: "Ramdom"): RamdomHandler;
+        getHandler(type: "Reversed"): ReversedHandler;
+        getHandler(type: string): Handler;
+        getHandler(type: string): Handler {
+            if (type === "Ramdom") {
+                return new RamdomHandler();
+            } else if (type === "Reversed") {
+                return new ReversedHandler();
+            } else {
+                return new Handler();
+            }
+        }
+    }
+
+    export class Handler {
+        handle() {
+            console.log("Handled!");
+        }
+    }
+
+    export class ReversedHandler extends Handler {
+        handle() {
+            console.log("!deldnaH");
+        }
+    }
+
+    export class RamdomHandler extends Handler {
+        handle() {
+            console.log("daH!denl");
+        }
+    }
+
+    export class RamdomHandler2 extends Handler {
+        handle() {
+            console.log("daH!denl2");
+        }
+    }
+}
+
+module Listing1_31 {
+
+    var addNumber = function (a: number, b: number) {
+        return a + b;
+    }
+
+    var addNumber = (a: number, b: number) => {
+        return a + b;
+    } 
+
+    var addNumber = (a: number, b: number) => a + b;
+}
+
+module Listing1_32 {
+    export var makeNum = (f: string, l: string) => ({ name: f, surname: l })
 }
 
 window.onload = () => {
@@ -123,8 +347,27 @@ window.onload = () => {
     console.log("nome: " + Listing1_6.person.name + " altezza: " + Listing1_6.person.heightInCentimeters);
     console.log("Edificio più alto: " + Listing1_7.tallestMonument.name);
     console.log("Edificio più alto: " + Listing1_7.tallestMonument.name);
-    console.log("Tipo di veicolo: " + Listing1_8.type);
-    console.log("Tipo di veicolo2: " + Listing1_8.typeName);
+    console.log("Index veicolo: " + Listing1_8.type);
+    console.log("Descrizione veicolo: " + Listing1_8.typeName);
+    console.log("Descrizione Box: " + Listing1_8.BoxSize[Listing1_8.BoxSize.ExtraLarge]);
+    console.log("flal1 :" + Listing1_10.hasD);
+    console.log("flal2 :" + Listing1_10.hasI);
+    console.log("flal3 :" + Listing1_10.hasS);
+    console.log("flal4 :" + Listing1_10.hasC);
+    console.log("mansion : " + Listing1_11.mansion);
+    console.log("invertedTest:" + Listing1_17.invertedTest);
+    console.log("truthyTest:" + Listing1_17.truthyTest);
+    console.log("falseyTest:" + Listing1_17.falseyTest);
+    Listing1_17.prova();
+    console.log(Listing1_25.result);
+    console.log(Listing1_25.result2);
+    console.log(Listing1_27.result);
+    var factory: Listing1_29.HandlerFactory = new Listing1_29.HandlerFactory();
+    console.log(factory.getHandler("Ramdom").handle);
+    console.log(factory.getHandler("Ramdom2").handle);
+
+    var person = Listing1_32.makeNum("Federico", "Salute");
+    console.log("Nome:" + person.name + ", cognome " + person.surname);
 };
 
 
